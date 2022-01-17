22e824be3ee2843a4d402836f548479cd42da03c
alexguliev591 <instagrammac1@gmail.com>
2022-01-17 18:20:16 +0300

    Laba 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+                double a = 6;
+
+                double b = 8;
+
+                double c;
+
+                c = Math.sqrt(a * a + b * b);
+
+                System.out.println("c = " + c);
+            }
+        }
